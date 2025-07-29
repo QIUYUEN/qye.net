8257b54377b6b9332d5daa1c4828981eef61bb1b
QYE <49049061+QIUYUEN@users.noreply.github.com>
2025-07-29 22:21:44 +0800

    Add files via upload

---
+++ js/scripts1.js
@@ -0,0 +1,19 @@
+// music
+document.addEventListener('click', function playMusic() {
+    const audio = new Audio('audio/bgm_1.mp3');
+    audio.loop = true;
+    audio.play().then(() => {
+        console.log('背景音乐已开始播放');
+    }).catch(error => {
+        console.error('自动播放失败：', error);
+    });
+    document.removeEventListener('click', playMusic);
+});
+
+// loading
+setTimeout(function() {
+  const msgDiv = document.getElementById("loading");
+  if (msgDiv) {
+    msgDiv.remove(); 
+  }
+}, 30000); 
